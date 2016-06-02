216fca6eae7fa36be801b01b73695793f4cc9385
Matt Szabo <wekilledit@users.noreply.github.com>
2016-06-02 14:29:53 +1000

    fixed spacing

--- app/components/Skills.jsx
+++ app/components/Skills.jsx
@@ -6,12 +6,9 @@ export default class Skills extends React.Component {
     const skillList = ['HTML', 'JavaScript', 'jQuery', 'AJAX', 'React', 'CSS'];
     return (
       <div>
-        <div>Hello</div>
-        <div>
-          <ul>
-            {skillList.map((topic, index) => <li key={index}>{topic}</li>)}
-          </ul>
-        </div>
+        <ul>
+          {skillList.map((topic, index) => <li key={index}>{topic}</li>)}
+        </ul>
       </div>
     );
   }
